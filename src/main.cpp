3cc7e7fb4844d30c48b89dd133fbb9f02d244dd5
pradlol <pradlol1337@gmail.com>
2018-10-26 01:55:16 +0700

    fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
 #include "objectsmodel.h"
+#include "polygonmodel.h"
 
 #include <QQuickView>
 #include <QQmlContext>
@@ -8,24 +8,23 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
 
     seye::ObjectModel model;
-    model.addObject(seye::Object(56.38943723960041, 85.21442028046647));
-    model.addObject(seye::Object(56.38866517548572, 85.2159223175081));
-    model.addObject(seye::Object(56.3867765218958, 85.2117595291229));
-    model.addObject(seye::Object(56.38634888921545, 85.21306844710358));
+    model.addObject(new seye::Object(56.38943723960041, 85.21442028046647));
+    model.addObject(new seye::Object(56.38866517548572, 85.2159223175081));
+    model.addObject(new seye::Object(56.3867765218958, 85.2117595291229));
+    model.addObject(new seye::Object(56.38634888921545, 85.21306844710358));
+
+    seye::PolygonModel polyModel;
 
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     QQmlContext* context = view.rootContext();
     context->setContextProperty("objectModel", &model);
+    context->setContextProperty("polygonModel", &polyModel);
 
     view.setSource(QUrl("qrc:/main.qml"));
     view.show();
-
-//    w.addModel("objectModel", &model);
-//    w.show();
 
     return a.exec();
 }
