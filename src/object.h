379dc3bf9eaeb1d4a122267121d1517f3588ba27
pradlol <pradlol1337@gmail.com>
2018-10-25 15:53:32 +0700

    Добавлены свойства объектам.
    Свойство для id.
    Свойство для currentCoordinate.

--- src/object.h
+++ src/object.h
@@ -6,13 +6,25 @@
 
 namespace seye
 {
-    class Object
+    class Object : public QObject
     {
+        Q_OBJECT
+        Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
+        Q_PROPERTY(QGeoCoordinate coordinate READ coordinate WRITE setCoordinate NOTIFY coordinateChanged)
+
     public:
         Object(double latitude, double longitude);
+        Object(QGeoCoordinate coord);
 
         int id() const;
+        void setId(int newId);
+
         QGeoCoordinate coordinate() const;
+        void setCoordinate(QGeoCoordinate newCoord);
+
+    signals:
+        void idChanged();
+        void coordinateChanged();
 
     private:
         int _id; // ?
