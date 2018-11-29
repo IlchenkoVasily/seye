624d704b87e1667450325e89ec2fa2e26bdfbf5f
VsevolodM <seva_makar@mail.ru>
2018-11-30 04:56:18 +0700

    Done

--- src/structs.h
+++ src/structs.h
@@ -5,12 +5,14 @@
 
 namespace seye
 {
-    struct passport
+    // все первые id, которые тут фигурируют, относятся к взаимодействию с бд, так что не следует их менять без участия функций dbservice
+    // так же это означает, что ориентироваться на них в качестве индекса QList тоже не следует
+    struct Passport
     {
-        qint64 id = 0; // ни в коем случае нигде его не меняем
-        QString firstName = "Имя";
-        QString lastName = "Фамилия";
-        QString callSign = "Позывной"; // 32 символа, уникально
+        qint64 id = 0;
+        QString firstName;
+        QString lastName;
+        QString callSign = "Позывной"; // максимум 32 символа, уникально
         QDate birthday = QDate::currentDate();
         QString device = nullptr; // либо пустое, либо уникальное значение из 16 символов
         // если задаваемое значение отсутствует в таблице объектов, то будет ошибка
@@ -23,15 +25,26 @@ namespace seye
                     + birthday.toString();
             return string;
         }
+        QString toString(const QString& idDevice) const // для попапа
+        {
+            if (idDevice == device)
+            {
+                QString string = firstName + " "
+                        + lastName + ", "
+                        + birthday.toString();
+                return string;
+            }
+            else return "У этого пасспорта не такой девайс";
+        }
     };
 
-    struct object
+    struct ObjectDev // Object уже есть, целый класс
     {
         QString id = "FFFFFFFFFFFFFFFF"; // 16 обязательных символов, уникально
         qint16 role; // по сути номер иконки, может быть пустым
         qint16 speedLimit = 25; // для проверки валидности координат
         // велечина, на которую за секунду может измениться расстояние в метрах
-        QString link = "Рупор"; // способ связи, 32 символа
+        QString link = "Рупор"; // способ связи, максимум 32 символа
         QString toString() const
         {
             QString string = id + ", "
@@ -42,11 +55,11 @@ namespace seye
         }
     };
     
-    struct zone
+    struct Zone
     {
-        qint32 id = 0; // ни в коем случае нигде его не меняем
-        QString name = "Имя";
-        QString polygon = "Координаты";
+        qint32 id = 0;
+        QString name;
+        QString polygon;
         QString color = "# и 8знач";
         QString lineColor = "# и 6зн";
         QString toString() const
@@ -60,13 +73,13 @@ namespace seye
         }
     };
 
-    struct access
+    struct Access
     {
-        qint64 id = 0; // ни в коем случае нигде его не меняем
-        QDateTime start = QDateTime::currentDateTime();
-        QDateTime end = QDateTime::currentDateTime();
-        QString priority = "Обычное"; // правило может быть: Обычное, Приоритетное, Неизменяемое
-        QString name = "Имя";
+        qint64 id = 0;
+        QDateTime start = QDateTime::fromString("2018-11-30 03:09:02", "yyyy-MM-dd hh:mm:ss");
+        QDateTime end = QDateTime::fromString("2018-11-30 03:09:02", "yyyy-MM-dd hh:mm:ss");
+        QString priority = "Обычное"; // возможные значения: Обычное, Приоритетное, Неизменяемое
+        QString name;
         qint64 group = 0; // id группы
         qint32 zone = 0; // id зоны
         QString toString() const
@@ -74,6 +87,7 @@ namespace seye
             QString string = QString::number(id) + ", "
                     + start.toString() + ", "
                     + end.toString() + ", "
+                    + priority + ", "
                     + name + ", "
                     + QString::number(group) + ", "
                     + QString::number(zone);
@@ -81,10 +95,10 @@ namespace seye
         }
     };
 
-    struct group
+    struct Group
     {
-        qint64 id = 0; // ни в коем случае нигде его не меняем
-        QString name = "Имя";
+        qint64 id = 0;
+        QString name; // должно быть уникальным
         QList<QString> devices; // список id объектов, состоящих в группе
         QString toString() const
         {
@@ -92,6 +106,25 @@ namespace seye
                     + name + ":";
             for(int i = 0; i < devices.size(); ++i) string += " " + devices[i];
             return string;
+        }
+    };
+
+    struct User
+    {
+        qint32 id;
+        QString name;
+        QString role; // возможные значения: operator, supervisor, admin
+        QString toString() const
+        {
+            QString string = QString::number(id) + ", "
+                    + name + ", "
+                    + role;
+            return string;
+        }
+        QString toString(const QString& roleName) const
+        {
+            if (roleName == role) return role;
+            else return "У этого пользователя не такая роль";
         }
     };
 }
